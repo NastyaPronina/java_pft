4200b8ea44ee473e13462c45a42ccd3b4a69a3e4
Anastasiya <nastyapro876@gmail.com>
2018-12-27 15:29:19 +0300

    Использование метода в классе Point

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -5,4 +5,16 @@ public class Point {
   public double x;
   public double y;
 
+  public Point (double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public double distance (Point point) {
+    return Math.sqrt(square(this.x - point.x) + square(this.y - point.y));
+  }
+
+  private double square (double a) {
+    return a*a;
+  }
 }
