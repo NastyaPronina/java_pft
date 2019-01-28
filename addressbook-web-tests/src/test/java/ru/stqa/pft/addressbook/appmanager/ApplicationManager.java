7a4d26addec3b452e53cd04a952c26d2b29afe57
Anastasiya <nastyapro876@gmail.com>
2019-01-28 13:42:54 +0300

    Добавлены проверки на определение количества групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -30,7 +30,7 @@ public class ApplicationManager {
     }
 
     wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-    wd.get("https://localhost/addressbook/group.php");
+    wd.get("https://localhost/addressbook");
     groupHelper = new GroupHelper(wd);
     navigationHelper = new NavigationHelper(wd);
     sessionHelper = new SessionHelper(wd);
