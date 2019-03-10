949cad5b7344168694b13c290add9f72d188fde8
NastyaPronina <nastyapro876@gmail.com>
2019-03-11 01:52:15 +0300

    Реализована интеграция тестов с баг-трекером Bugify

--- rest-sample/src/test/java/ru/stqa/pft/rest/RestAssuredTests.java
+++ rest-sample/src/test/java/ru/stqa/pft/rest/RestAssuredTests.java
@@ -1,13 +1,6 @@
 package ru.stqa.pft.rest;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
-import com.google.gson.reflect.TypeToken;
 import com.jayway.restassured.RestAssured;
-import org.apache.http.client.fluent.Executor;
-import org.apache.http.client.fluent.Request;
-import org.apache.http.message.BasicNameValuePair;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -16,7 +9,7 @@ import java.util.Set;
 
 import static org.testng.Assert.assertEquals;
 
-public class RestAssuredTests {
+public class RestAssuredTests extends TestBase {
 
   @BeforeClass
   public void init() {
@@ -25,27 +18,12 @@ public class RestAssuredTests {
 
   @Test
   public void testCreateIssue() throws IOException {
+    skipIfNotFixed(525);
     Set<Issue> oldIssues = getIssues();
     Issue newIssue = new Issue().withSubject("Test issue").withDescription("New test issue");
     int issueId = createIssue(newIssue);
     Set<Issue> newIssues = getIssues();
     oldIssues.add(newIssue.withId(issueId));
     assertEquals(newIssues, oldIssues);
-  }
-
-  private Set<Issue> getIssues() throws IOException {
-    String json = RestAssured.get("http://bugify.stqa.ru/api/issues.json?limit=1000").asString();
-    JsonElement parsed = new JsonParser().parse(json);
-    JsonElement issues = parsed.getAsJsonObject().get("issues");
-    return new Gson().fromJson(issues, new TypeToken<Set<Issue>>(){}.getType());
-  }
-
-  private int createIssue(Issue newIssue) throws IOException {
-    String json = RestAssured.given()
-        .parameter("subject", newIssue.getSubject())
-        .parameter("description", newIssue.getDescription())
-        .post("http://bugify.stqa.ru/api/issues.json").asString();
-    JsonElement parsed = new JsonParser().parse(json);
-    return parsed.getAsJsonObject().get("issue_id").getAsInt();
   }
 }
